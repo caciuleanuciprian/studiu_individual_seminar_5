db90bdec23726fbfb15bb7b4c9b416e657a0768c
Ciprian <56521866+caciuleanuciprian@users.noreply.github.com>
2020-11-11 21:34:27 +0200

    Add files via upload

---
+++ StudiuIndividualProblema5/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudiuIndividualProblema5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n;
+            bool mhm = true;
+
+            Console.WriteLine("n= ");
+            n = int.Parse(Console.ReadLine());
+
+
+            int[] array = new int[n];
+
+            Console.WriteLine("Elementele vectorului: ");
+
+            int i = 0;
+
+            while (i < n)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+                i++;
+            }
+
+            i = 1;
+
+            while (i < n)
+            {
+                if (array[i - 1] < array[i])
+                {
+                    mhm = false;
+                }
+
+                i++;
+            }
+
+            if (mhm == true)
+            {
+                Console.WriteLine("Este crescator de la dreapta la stanga.");
+            }
+            else
+            {
+                Console.WriteLine("Nu este crescator de la dreapta la stanga.");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
